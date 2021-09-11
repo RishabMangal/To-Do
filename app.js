ed265a247c1a33d3348dbca74af6c557c2311474
RishabMangal <rishabhmangal1@gmail.com>
2021-09-11 17:32:07 +0530

    db updated

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var todoController = require('./controllers/todoController');
-var app = express();
-var path = require('path');
-app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "views"));
+var express = require('express')
+var todoController = require('./controllers/todoController')
+var app = express()
+var path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static('public'));
-app.use(express.static('views'));
+app.use(express.static('public'))
+app.use(express.static('views'))
 
+todoController(app)
 
-todoController(app);
+const PORT = process.env.PORT || 3000
 
 // var code="<button onclick=\"console.log('Fuck yeah!!');req.url=res.url+'/todo';\">Click Me</button>"
-var code = "<link rel='stylesheet' type='text/css' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'/><body class='bg-dark'><h1 class='display-4 p-3 text-secondary text-center text-capitalize font-italic font-weight-bold'>welcome</h1><a href='/todo'><button class='btn btn-primary m-3'>To-Do App</button></a> <div class='mr-auto container-fluid fixed-bottom ' style='background-color:black'><p class='text-center px-3 mr-auto text-secondary'>Thank You for Visiting</p><p class='text-secondary text-right px-3 mr-auto'>Developed by- Rishab Mangal'</p><p class='text-secondary text-right text-weight-light px-3 mr-auto'>Version 1.0.0</p></div></body>";
-app.get("/",function(req,res) {
-    // res.send("Go to localhost:3000/todo");
-    // req.url=res.url+"/todo";.
-    res.writeHead(200,{"Content-Type":"text/html"});
-    // ;.
-    res.end(code);
-    
+var code =
+  "<link rel='stylesheet' type='text/css' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'/><body class='bg-dark'><h1 class='display-4 p-3 text-secondary text-center text-capitalize font-italic font-weight-bold'>welcome</h1><a href='/todo'><button class='btn btn-primary m-3'>To-Do App</button></a> <div class='mr-auto container-fluid fixed-bottom ' style='background-color:black'><p class='text-center px-3 mr-auto text-secondary'>Thank You for Visiting</p><p class='text-secondary text-right px-3 mr-auto'>Developed by- Rishab Mangal'</p><p class='text-secondary text-right text-weight-light px-3 mr-auto'>Version 1.0.0</p></div></body>"
+app.get('/', function (req, res) {
+  // res.send("Go to localhost:3000/todo");
+  // req.url=res.url+"/todo";.
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  // ;.
+  res.end(code)
 })
 
-app.listen(3000);
-console.log("Go to port 3000");
+app.listen(PORT, () => console.log('Go to port ' + PORT))
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,9 +10,9 @@
 //   { item: "Repeat" }
 // ];
 
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser')
 
-var urlencoded = bodyParser.urlencoded({ extended: false });
+var urlencoded = bodyParser.urlencoded({ extended: false })
 
 // var mongoose = require("mongoose");
 
@@ -40,13 +40,14 @@ var urlencoded = bodyParser.urlencoded({ extended: false });
 //     console.log("Item Saved To DB");
 // });
 
-var MongoClient = require("mongodb").MongoClient;
+var MongoClient = require('mongodb').MongoClient
+// var url ="mongodb+srv://todo:test@todo-gizyq.mongodb.net/test?retryWrites=true&w=majority";
 var url =
-  "mongodb+srv://test:test@todo-gizyq.mongodb.net/test?retryWrites=true&w=majority";
+  'mongodb+srv://rishabmangal1:Mangal@1999@backmeup.zyrtp.mongodb.net/todo?retryWrites=true&w=majority'
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("todo");
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err
+  var dbo = db.db('todo')
   //   var myobj = { item: "Yipee I am Added" };
   //   var myobj2 = { item: "Yipee I am Added again" };
   //   dbo.collection("todo-list").insertMany(todos, function(err, res) {
@@ -55,17 +56,17 @@ MongoClient.connect(url, function(err, db) {
   //     db.close();
   //   });
   dbo
-    .collection("todo-list")
+    .collection('todo-list')
     .find({})
-    .toArray(function(err, res) {
-      if (err) throw err;
-      console.log("List of All Items:");
-      console.log(res);
-      console.log("----------------------------------");
-      db.close();
-    });
+    .toArray(function (err, res) {
+      if (err) throw err
+      console.log('List of All Items:')
+      console.log(res)
+      console.log('----------------------------------')
+      db.close()
+    })
   // db.close();
-});
+})
 
 // MongoClient.connect(url, function(err, db) {
 //   if (err) throw err;
@@ -78,71 +79,80 @@ MongoClient.connect(url, function(err, db) {
 //   });
 // });
 
-module.exports = function(app) {
-  app.get("/todo", function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("todo");
-      dbo.collection("todo-list").find({}).toArray(function(err, result) {
-          if (err) throw err;
-          console.log(result);
-          res.render("todo", { todos: result });
-          db.close();
-        });
+module.exports = function (app) {
+  app.get('/todo', function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err
+      var dbo = db.db('todo')
+      dbo
+        .collection('todo-list')
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw err
+          console.log(result)
+          res.render('todo', { todos: result })
+          db.close()
+        })
       // db.close();
-    });
-  });
+    })
+  })
 
-  app.post("/todo", urlencoded, function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("todo");
-      dbo.collection("todo-list").insertOne(req.body, function(err, res) {
-        if (err) throw err;
-        console.log("Your Item Added to DB");
-        console.log(req.body);
-        db.close();
-      });
-      dbo.collection("todo-list").find({}).toArray(function(err, result) {
-          if (err) throw err;
-          console.log("Data Sended to Json");
-          res.json(result);
-          db.close();
-        });
-    });
+  app.post('/todo', urlencoded, function (req, res) {
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err
+      var dbo = db.db('todo')
+      dbo.collection('todo-list').insertOne(req.body, function (err, res) {
+        if (err) throw err
+        console.log('Your Item Added to DB')
+        console.log(req.body)
+        db.close()
+      })
+      dbo
+        .collection('todo-list')
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw err
+          console.log('Data Sended to Json')
+          res.json(result)
+          db.close()
+        })
+    })
     // todos.push(req.body);
     // res.json(todos);
-  });
+  })
 
-  app.delete("/todo/:item", function(req, res) {
+  app.delete('/todo/:item', function (req, res) {
     // todos = todos.filter(function(todo){
     //     return todo.item.replace(/ /g,'-') !== req.params.item;
     // });
-    console.log(req.params.item);
+    console.log(req.params.item)
     // todos = todos.filter(x => {
     //   return x.item !== req.params.item;
     // });
     // res.json(todos);
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("todo");
-      var myquery = { item: req.params.item };
-      dbo.collection("todo-list").deleteOne(myquery, function(err, result) {
-        if (err) throw err;
-        console.log("One Item Deleted " + req.params.item);
-        db.close();
-      });
-    });
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err
+      var dbo = db.db('todo')
+      var myquery = { item: req.params.item }
+      dbo.collection('todo-list').deleteOne(myquery, function (err, result) {
+        if (err) throw err
+        console.log('One Item Deleted ' + req.params.item)
+        db.close()
+      })
+    })
 
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("todo");
-      dbo.collection("todo-list").find({}).toArray(function(err, result) {
-          if (err) throw err;
-          console.log("Deleted Item And Data Sent to Json");
-          res.json(result);
-          db.close();
-        });
-    });
-  });
-};
+    MongoClient.connect(url, function (err, db) {
+      if (err) throw err
+      var dbo = db.db('todo')
+      dbo
+        .collection('todo-list')
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw err
+          console.log('Deleted Item And Data Sent to Json')
+          res.json(result)
+          db.close()
+        })
+    })
+  })
+}
